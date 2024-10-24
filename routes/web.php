a5b6c02646db6ef359f7635db037a2765b8a2004
Kavidu M Kulathunga <154060942+KaviduMalshan99@users.noreply.github.com>
2024-10-24 11:29:25 +0530

    update theme

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminTemplateController;
+use App\Http\Controllers\HomeTemplateController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/admin',[AdminTemplateController::class,'index']);
+Route::get('/home',[HomeTemplateController::class,'index']);
 
 require __DIR__.'/auth.php';
