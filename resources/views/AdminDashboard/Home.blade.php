489f6a6985a55dd8102e740903379b582e447a4f
Kavidu M Kulathunga <154060942+KaviduMalshan99@users.noreply.github.com>
2024-10-24 13:07:47 +0530

    main update

---
+++ resources/views/AdminDashboard/Home.blade.php
@@ -0,0 +1,1 @@
+@extends ('AdminDashboard.master')
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminTemplateController;
+use App\Http\Controllers\HomeTemplateController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/admin',[AdminTemplateController::class,'index']);
+Route::get('/home',[HomeTemplateController::class,'index']);
+
+require __DIR__.'/auth.php';
