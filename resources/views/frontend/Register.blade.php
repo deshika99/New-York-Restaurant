fc3f76f09f9d7f9278932adeeba14d0359f0643a
MalkiMadu <malkimadushani353@gmail.com>
2024-11-01 09:36:34 +0530

    Update  Register and login page.

---
+++ resources/views/frontend/Register.blade.php
@@ -0,0 +1,167 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Quarter - Real Estate HTML Template</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Place favicon.png in the root directory -->
+    <link rel="shortcut icon" href="frontend/img/favicon.png" type="image/x-icon" />
+    <!-- Font Icons css -->
+    <link rel="stylesheet" href="frontend/css/font-icons.css">
+    <!-- plugins css -->
+    <link rel="stylesheet" href="frontend/css/plugins.css">
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="frontend/css/style.css">
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="frontend/css/responsive.css">
+</head>
+
+<body>
+    <!--[if lte IE 9]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+
+    <!-- Add your site or application content here -->
+
+<!-- Body main wrapper start -->
+<div class="body-wrapper">
+
+    <!-- HEADER AREA START (header-5) -->
+    @include('frontend.header')
+    <!-- Utilize Mobile Menu End -->
+
+    <div class="ltn__utilize-overlay"></div>
+
+    <!-- BREADCRUMB AREA START -->
+    <div class="ltn__breadcrumb-area text-left bg-overlay-white-30 bg-image "  data-bs-bg="frontend/img/bg/14.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ltn__breadcrumb-inner">
+                        <h1 class="page-title">Account</h1>
+                        <div class="ltn__breadcrumb-list">
+                            <ul>
+                                <li><a href="index.html"><span class="ltn__secondary-color"><i class="fas fa-home"></i></span> Home</a></li>
+                                <li>Register</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- BREADCRUMB AREA END -->
+
+    <!-- LOGIN AREA START (Register) -->
+
+    <div class="ltn__login-area pb-110">
+        @if (session()->has("success"))
+    <div class="alert alert-warning alert-dismissible fade show" role="alert" width="300px">
+        {{ session()->get("success") }}
+    </div>
+@endif
+@if (session()->has("error"))
+    <div class="alert alert-warning alert-dismissible fade show" role="alert" width="300px">
+        {{ session()->get("error") }}
+    </div>
+@endif
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title-area text-center">
+                        <h1 class="section-title">Register <br>Your Account</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br>
+                             Sit aliquid,  Non distinctio vel iste.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="account-login-inner">
+                        <form action="{{route('registerpage.user')}}" method="post" class="ltn__form-box contact-form-box">
+                        @csrf
+                            <input type="text" name="fname" placeholder="First Name">
+                            <input type="text" name="lname" placeholder="Last Name">
+                            <input type="text" name="email" placeholder="Email*">
+                            <input type="text" name="phone_number" placeholder="Phone Number">
+                            <input type="text" name="address" placeholder="Address">
+                            <input type="password" name="password" placeholder="Password*">
+                           <!-- <input type="password" name="confirmpassword" placeholder="Confirm Password*">-->
+                            <label class="checkbox-inline">
+                                <input type="checkbox" value="">
+                                I consent to Herboil processing my personal data in order to send personalized marketing material in accordance with the consent form and the privacy policy.
+                            </label>
+                            <label class="checkbox-inline">
+                                <input type="checkbox" value="">
+                                By clicking "create account", I consent to the privacy policy.
+                            </label>
+                            <div class="btn-wrapper">
+                                <button class="theme-btn-1 btn reverse-color btn-block" type="submit">CREATE ACCOUNT</button>
+                            </div>
+                        </form>
+                        <div class="by-agree text-center">
+                            <p>By creating an account, you agree to our:</p>
+                            <p><a href="#">TERMS OF CONDITIONS  &nbsp; &nbsp; | &nbsp; &nbsp;  PRIVACY POLICY</a></p>
+                            <div class="go-to-btn mt-50">
+                                <a href="login.html">ALREADY HAVE AN ACCOUNT ?</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- LOGIN AREA END -->
+
+    <!-- CALL TO ACTION START (call-to-action-6) -->
+    <div class="ltn__call-to-action-area call-to-action-6 before-bg-bottom" data-bs-bg="frontend/img/1.jpg--">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="call-to-action-inner call-to-action-inner-6 ltn__secondary-bg position-relative text-center---">
+                        <div class="coll-to-info text-color-white">
+                            <h1>Looking for a dream home?</h1>
+                            <p>We can help you realize your dream of a new home</p>
+                        </div>
+                        <div class="btn-wrapper">
+                            <a class="btn btn-effect-3 btn-white" href="contact.html">Explore Properties <i class="icon-next"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- CALL TO ACTION END -->
+
+    <!-- FOOTER AREA START -->
+    @include('frontend.footer')
+    <!-- FOOTER AREA END -->
+
+</div>
+<!-- Body main wrapper end -->
+
+    <!-- All JS Plugins -->
+    <script src="frontend/js/plugins.js"></script>
+    <!-- Main JS -->
+    <script src="frontend/js/main.js"></script>
+
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminTemplateController;
 use App\Http\Controllers\HomeTemplateController;
 use App\Http\Controllers\ContactTemplateController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\RegisterController;
 
 
 Route::get('/', function () {
@@ -22,10 +24,12 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/admin',[AdminTemplateController::class,'index']);
-Route::get('/home',[HomeTemplateController::class,'index']);
-Route::get('/contact',[ContactTemplateController::class,'index']);
+Route::get('/home',[HomeTemplateController::class,'index'])->name('home');
+Route::get('/contact',[ContactTemplateController::class,'index'])->name('contact');
+Route::get('/about',[AboutController::class,'index'])->name('about');
+Route::get('/registerpage',[RegisterController::class,'index'])->name('registerpage');
+Route::post('/registerpage',[RegisterController::class,'register'])->name('registerpage.user');
+Route::get('/loginpage',[RegisterController::class,'login'])->name('loginpage');
 
-Route::get('/about', function () {
-    return view('frontend.AboutUs');
-});
+
 require __DIR__.'/auth.php';
